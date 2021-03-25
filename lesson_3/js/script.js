58e2b754488a1e8cecfecec78ca20e591f686ce8
Sulan <sultanu78@mail.ru>
2021-03-25 18:56:28 +0500

    Прошел урок по фунциям

--- lesson_3/js/script.js
+++ lesson_3/js/script.js
@@ -1,5 +1,17 @@
-function myFunction() {
-    alert('Done');
-}
+// function myFunction() {
+//     alert('Done');
+// }
 
-myFunction();
+// myFunction();
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+// console.log(calc(3, 4));
+
+let str = 'test';
+
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
